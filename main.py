68d8e10150198daf4b0dd256d29928de132ef18f
manjushree-07 <shreekochrekar15@gmail.com>
2024-05-22 11:22:43 +0530

    Implemented add, substract funcitonality

---
+++ main.py
@@ -0,0 +1,7 @@
+#from math_ops import add_numbers
+import math_ops
+
+# Call the add_numbers function with two numbers and print the result
+result = math_ops.add_numbers(5, 3)
+print(f"The result of adding 5 and 3 is: {result}")
+print(f"The result of subtracting 5 and 3 is: {math_ops.subtract(5,3)}")
---
+++ math_ops.py
@@ -0,0 +1,7 @@
+def add_numbers(a, b):
+    """Function to add two numbers and return the result."""
+    return a + b
+
+def subtract(a, b):
+    """Function to add two numbers and return the result."""
+    return a - b
