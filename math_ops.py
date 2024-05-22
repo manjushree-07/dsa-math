4e90fc94bc22abfca3bfcc5ca0111ddf1b236223
manjushree-07 <shreekochrekar15@gmail.com>
2024-05-22 11:44:02 +0530

    added multiply functionality

--- math_ops.py
+++ math_ops.py
@@ -5,3 +5,7 @@ def add_numbers(a, b):
 def subtract(a, b):
     """Function to add two numbers and return the result."""
     return a - b
+
+def multiply(a, b):
+    """Function to add two numbers and return the result."""
+    return a * b
